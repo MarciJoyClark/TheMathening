3ad235ca6f70c2be0f2040d0fbf737ac05ea9e11
MarciJoyClark <33448580+MarciJoyClark@users.noreply.github.com>
2018-05-20 23:09:37 -0700

    Upload
    This SHOULD be everything

---
+++ Math_fnc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReMath
+{
+    public class Math_fnc
+    {
+        public static int AddFirst(int numberOne)
+            {
+                int numberAdd = numberOne + 13;
+                return numberAdd;
+            }
+        public static int Subtract(int numberTwo)
+            {
+                int numberSub = numberTwo - 4;
+                return numberSub;
+            }
+        public static int AddSecond(int numberThree)
+            {
+                int numberAdd2 = numberThree + 9;
+                return numberAdd2;
+            }
+    }
+}
---
+++ ReMath/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReMath
+{
+    public class Program
+    { 
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a positive, whole number to run some math on.");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your input was " + userInput + ".");
+            int variable = Math_fnc.AddFirst(userInput);
+            Console.WriteLine("Your number plus 13 = " + variable);
+            Console.ReadLine();
+            Console.WriteLine("Give me another positive, whole number to run some more math on.");
+            int userInput2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your input was " + userInput2 + ".");
+            int variable2 = Math_fnc.Subtract(userInput2);
+            Console.WriteLine("Your number - 4 = " + variable2);
+            Console.ReadLine();
+            Console.WriteLine("How abouts OOOONE more?");
+            int userInput3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your input was " + userInput3 + ".");
+            int variable3 = Math_fnc.AddSecond(userInput3);
+            Console.WriteLine("Your number plus 9 = " + variable3);
+            Console.ReadLine();
+        }
+    }
+}
+
